72e9e8333e1304a873e5c0b72bc1ad2bee4d43be
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-02-03 23:34:10 +0000

    imports to sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,22 @@
+import { 
+  Box,
+  List,
+  ListItem, 
+  ListIcon,
+  LinkBox, 
+  LinkOverlay,
+  Divider,
+  Center
+ } from "@chakra-ui/layout";
+ import { 
+   MdHome,
+   
+  } from "react-icons/md";
+
+const Sidebar = () => {
+  return (
+    <h1>hello from sidebar</h1>
+  )
+}
+
+export default Sidebar
