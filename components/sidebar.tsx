707c44d318bd5ae3f22f35b4d95b1a61e54ba145
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-02-04 00:47:07 +0000

    navitems content done

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,5 @@
 import NextImage from "next/image";
+import NextLink from "next/link";
 import { 
   Box,
   List,
@@ -44,9 +45,31 @@ const Sidebar = () => {
       paddingX='5px'
       color='gray'
     >
+      {/* Content Box */}
       <Box paddingY='20px'>
+        {/* ImageBox */}
         <Box width='120px' marginBottom='20px' paddingX='20px' >
           <NextImage src='/logo.svg' height={60} width={120}/>
+        </Box>
+
+        {/* NavItems Content */}
+        <Box marginBottom='20px' >
+          <List spacing={2} >
+            {
+              navMenuItems.map(item => (
+                <ListItem paddingX='20px' fontSize='16px' key={item.name} >
+                  <LinkBox>
+                    <NextLink href={item.route} passHref >
+                      <LinkOverlay>
+                        <ListIcon as={item.icon} color='white' marginRight='20px' />
+                        { item.name }
+                      </LinkOverlay>
+                    </NextLink>
+                  </LinkBox>        
+                </ListItem>
+              ))
+            }
+          </List>
         </Box>
       </Box>
     </Box>
