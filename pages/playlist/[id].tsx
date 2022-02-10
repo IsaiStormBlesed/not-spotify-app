f77835aa64cdf3ce665953ace77c6cce1760d235
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-02-10 01:51:28 +0000

    more work on playlist

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,8 +1,37 @@
+import GradientLayout from '../../components/gradientLayout'
 import { validateToken } from '../../lib/auth'
 import prisma from '../../lib/prisma'
 
+const getBGColors = (id) => {
+  const colors = [
+    'red',
+    'blue',
+    'green',
+    'orange',
+    'purple',
+    'gray',
+    'yellow',
+    'teal',
+  ]
+
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)]
+}
+
 const Playlist = ({ playlist }) => {
-  return <h1>{playlist.name}</h1>
+  const color = getBGColors(playlist.id)
+
+  return (
+    <GradientLayout
+      color={color}
+      title={playlist.name}
+      subtitle="playlist"
+      description={`${playlist.songs.length} songs`}
+      roundImage={false}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+    >
+      <h1>{playlist.name}</h1>
+    </GradientLayout>
+  )
 }
 
 export const getServerSideProps = async ({ query, req }) => {
